b08ecedbbc6fdf001724594efb1620c07cf482cc
Devang Mishra <devangmshr@gmail.com>
2025-12-11 17:56:32 +0530

    Student counter in record added

--- Practice/src/Advance_Java/Record/OldDeclarationAndNewDeclaration.java
+++ Practice/src/Advance_Java/Record/OldDeclarationAndNewDeclaration.java
@@ -3,17 +3,22 @@ package Advance_Java.Record;
 import java.util.Objects;
 
 public class OldDeclarationAndNewDeclaration {
-   static class Student{                                    //old format
+    static class Student { // old format
         private final String name;
         private final int age;
-        public Student(String name, int age){
-            this.name=name;
-            this.age=age;
+
+        public Student(String name, int age) {
+            this.name = name;
+            this.age = age;
         }
 
-        public String getName(){return name;}
+        public String getName() {
+            return name;
+        }
 
-        public int getAge(){return age;}
+        public int getAge() {
+            return age;
+        }
 
         @Override
         public String toString() {
@@ -23,25 +28,43 @@ public class OldDeclarationAndNewDeclaration {
                     '}';
         }
 
-       @Override
-       public boolean equals(Object o) {
-           if (o == null || getClass() != o.getClass()) return false;
-           Student student = (Student) o;
-           return age == student.age && Objects.equals(name, student.name);
-       }
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass())
+                return false;
+            Student student = (Student) o;
+            return age == student.age && Objects.equals(name, student.name);
+        }
 
-       @Override
-       public int hashCode() {
-           return Objects.hash(name, age);
-       }
-   }
-   Student student=new Student("dev",25);
-   Student student1=new Student("dev",24);
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, age);
+        }
+    }
+
+    Student student = new Student("dev", 25);
+    Student student1 = new Student("dev", 24);
 }
-record StudentGen(String name, int age){}                    //new format
-class NewDeclaration{
+
+record StudentGen(String name, int age) {
+    private static int studentcount;
+
+    public StudentGen {
+        studentcount++;
+    }
+
+    public int getStudentcount() {
+        return studentcount;
+    }
+} // new format
+
+class NewDeclaration {
     static void main() {
-        StudentGen s1=new StudentGen("dev",25);
+        StudentGen s1 = new StudentGen("dev", 25);
         System.out.println(s1);
+        System.out.println(s1.getStudentcount());
+        StudentGen s2 = new StudentGen("dev", 20);
+        System.out.println(s2);
+        System.out.println(s2.getStudentcount());
     }
 }
