917695e068789e9031bc7cb9dd5db4d3402d76a7
Devang Mishra <devangmshr@gmail.com>
2025-12-11 16:02:52 +0530

    Stream API, Threads implemented

--- Practice/src/Advance_Java/Concurrency/DemoThreads.java
+++ Practice/src/Advance_Java/Concurrency/DemoThreads.java
@@ -1,20 +1,36 @@
 package Advance_Java.Concurrency;
 
 public class DemoThreads {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Counter counter = new Counter(); // Create an instance
         Runnable obja = () -> {
-            for (int i = 0; i <= 1000; ++i) {
-                System.out.println("Thread A: " + i);
+            for (int i = 0; i < 10000; ++i) {
+                counter.increment(); // Call on instance
             }
         };
         Runnable objb = () -> {
-            for (int i = 0; i <= 1000; ++i) {
-                System.out.println("Thread B: " + i);
+            for (int i = 0; i < 10000; ++i) {
+                counter.increment(); // Call on instance
             }
         };
         Thread t1 = new Thread(obja);
         Thread t2 = new Thread(objb);
         t1.start();
         t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(counter.count);
+    }
+}
+
+class Counter {
+    int count;
+
+    public synchronized void increment() {
+        count++;
     }
 }
